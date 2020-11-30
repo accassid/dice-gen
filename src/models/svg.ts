2195aa87bd6e846a0703739138c0facb21955c62
accassid <accassid@buffalo.edu>
2020-11-30 17:08:53 -0500

    * Adding SVG position options to SVG data type * Adding controls for x and y * Adding filename SVG type and setting it on file change

--- src/models/svg.ts
+++ src/models/svg.ts
@@ -2,9 +2,15 @@ export type SVGType = {
   scale: number
   rotation: number
   primitiveMesh: object | null
+  x: number
+  y: number
+  fileName: string | null
 }
 export const DEFAULT_SVG = {
   scale: 0.7,
   rotation: 0,
   primitiveMesh: null,
+  x: 0,
+  y: 0,
+  fileName: null,
 }
